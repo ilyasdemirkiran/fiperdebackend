da4c96cf6b3ee97ab86dd5ebd54a7cace064e296
ilyasdemirkiran <dkilyas@gmail.com>
2026-02-26 01:53:37 +0300

    vendor price rate max 500

--- src/types/vendor/vendor_price_rate.ts
+++ src/types/vendor/vendor_price_rate.ts
@@ -1,14 +1,10 @@
 import { z } from 'zod';
-import { phoneNumberSchema } from '@/types/phone_number';
-import { isEmpty } from 'es-toolkit/compat';
-import { timestampSchema } from '@/types/timestamp';
 import { ObjectId } from 'mongodb';
 
-// Base object schema without refinements
 export const vendorPriceRateSchema = z.object({
   _id: z.custom<ObjectId>().optional(),
   vendorId: z.custom<ObjectId>(),
-  rate: z.number().min(0).max(100),
+  rate: z.number().min(0).max(500).default(0),
 });
 
 export type VendorPriceRate = z.infer<typeof vendorPriceRateSchema>;
